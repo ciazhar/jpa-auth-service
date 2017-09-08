06bf5a0d2e21f17c2730d1acacf3b21422c169cc
ciazhar <daisetsunakama@gmail.com>
2017-09-08 16:56:01 +0700

    add permission form

---
+++ src/main/kotlin/com/ciazhar/authserver/model/dto/request/PermissionForm.kt
@@ -0,0 +1,13 @@
+package com.ciazhar.authserver.model.dto.request
+
+/**
+ * Created by ciazhar on 9/8/17.
+ */
+
+class PermissionForm(
+
+        var id : String ?=null,
+        var id_role : String?=null,
+        var nama: String?=null,
+        var label:String?=null
+)
