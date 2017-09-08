663ed9bcd127de727a6948caed8ba5efcd727daf
ciazhar <daisetsunakama@gmail.com>
2017-09-08 11:30:05 +0700

    update permission service

--- src/main/kotlin/com/ciazhar/authserver/service/impl/PermissionServiceImpl.kt
+++ src/main/kotlin/com/ciazhar/authserver/service/impl/PermissionServiceImpl.kt
@@ -11,34 +11,27 @@ import java.util.*
 @Service
 class PermissionServiceImpl (private val permissionRepository: PermissionRepository): PermissionService {
     override fun save(permission: Permission): ResponseData<*> {
-        try {
-            return ResponseData(permissionRepository.save(permission))
-        } catch (e: Exception) {
-            return ResponseData<Objects>(status = ErrorMessage.ERROR,message = e.message)
+        if(permission.id!=null&&permissionRepository.findOne(permission.id)==null){
+            return ResponseData<Objects>(status = "Update Failed",message = "ID Not Found")
         }
+        return ResponseData(permissionRepository.save(permission))
     }
 
     override fun findAll(): ResponseData<*> {
-        try {
-            return ResponseData(permissionRepository.findAll())
-        } catch (e: Exception) {
-            return ResponseData<Objects>(status = ErrorMessage.ERROR,message = e.message)
-        }
+        return ResponseData(permissionRepository.findAll())
     }
 
     override fun findOne(id: String): ResponseData<*> {
-        try {
-            return ResponseData(permissionRepository.findOne(id))
-        } catch (e: Exception) {
-            return ResponseData<Objects>(status = ErrorMessage.ERROR,message = e.message)
-        }
+        val permission = permissionRepository.findOne(id) ?: return ResponseData<Objects>(status = "Update Failed",message = "ID Not Found")
+
+        return ResponseData(permission)
     }
 
     override fun delete(id: String): ResponseData<*> {
-        try {
-            return ResponseData(permissionRepository.delete(id))
-        } catch (e: Exception) {
-            return ResponseData<Objects>(status = ErrorMessage.ERROR,message = e.message)
-        }
+        val role = permissionRepository.findOne(id) ?: return ResponseData<Objects>(status = "Update Failed",message = "ID Not Found")
+
+        permissionRepository.delete(id)
+
+        return ResponseData(role)
     }
 }
