0baae355b5ec046fe7198fd48184687a3a3361b5
ciazhar <daisetsunakama@gmail.com>
2017-09-09 20:57:13 +0700

    migrasi api dll

--- src/main/kotlin/com/ciazhar/authserver/service/EmailService.kt
+++
@@ -1,9 +0,0 @@
-package com.ciazhar.authserver.service
-
-import com.ciazhar.authserver.model.dto.request.RegisterForm
-import com.ciazhar.authserver.util.email.EmailStatus
-
-interface EmailService {
-
-    fun sendEmail(form: RegisterForm): EmailStatus
-}
